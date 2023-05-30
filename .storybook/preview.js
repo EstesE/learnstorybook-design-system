9973d53c59306b7f8f30a35be64763fec4d64afa
Mithrilhall <eric_estes@aspensquare.com>
2023-05-30 15:05:46 -0400

    Add actions and controls

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,23 @@
-/** @type { import('@storybook/react').Preview } */
-const preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
+import React from "react";
+
+import { GlobalStyle } from "../src/shared/global";
+
+// Global decorator to apply the styles to all stories
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  ),
+];
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
     },
   },
 };
-
-export default preview;
